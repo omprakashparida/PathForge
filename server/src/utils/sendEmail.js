96d8b8abffb91d9eb74ca471a58a4f26881242ef
Om Prakash Parida <179730524+omprakashparida@users.noreply.github.com>
2026-05-25 20:21:08 +0530

    Force IPv4 for Gmail SMTP

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,15 +1,24 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+// Force IPv4
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
 
 host:"smtp.gmail.com",
 
 port:587,
+
 secure:false,
 
 auth:{
 user:process.env.EMAIL_USER,
 pass:process.env.EMAIL_PASS
+},
+
+tls:{
+rejectUnauthorized:false
 }
 
 });
